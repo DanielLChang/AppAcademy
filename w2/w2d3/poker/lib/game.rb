0b0d82a71f2b67c6977c7b332b73a413455c37eb
DanielLChang <daniellawrencechang@gmail.com>
2016-11-27 16:19:36 -0800

    finished poker game.

--- w2/w2d3/poker/lib/game.rb
+++ w2/w2d3/poker/lib/game.rb
@@ -11,39 +11,116 @@ class Game
   end
 
   def play
+    play_round until game_over?
   end
 
   def play_round
+    @deck.shuffle
+    unfold_players
+    deal_cards
+    take_bets
+    if game_over?
+      end_round
+      return
+    end
+    trade_cards
+    take_bets
+    end_round
   end
 
   def unfold_players
+    @players.each(&:unfold)
   end
 
   def deal_cards
+    @players.each do |player|
+      next if player.folded?
+      player.deal_in(@deck.deal_hand)
+    end
   end
 
   def take_bets
+    high_bet = 0
+    no_raises = false
+    pivoted_players = @players.dup
+    pivot_point = 0
+    betted_player = nil
+
+    until no_raises
+      no_raises = true
+
+      pivoted_players.each_with_index do |player, idx|
+        next if player.folded?
+        next if betted_player == player
+        puts "High bet: $#{high_bet}"
+        puts "Player #{idx + 1}:"
+        puts player.hand
+        response = player.respond_bet
+        case response
+        when :call
+          player.take_bet(high_bet)
+          add_to_pot(high_bet)
+        when :bet
+          no_raises = false
+          pivot_point = idx
+          betted_player = player
+          bet = player.get_bet
+          raise "bet must be at least $#{high_bet}" if bet < high_bet
+          high_bet = add_to_pot(player.take_bet(bet))
+        when :fold
+          player.fold
+        end
+
+        return if game_over?
+      end
+
+    end
   end
 
   def trade_cards
+    @players.each_with_index do |player, idx|
+      next if player.folded?
+      puts "Player #{idx + 1}:"
+      puts player.hand
+      cards = player.get_cards_to_trade
+      @deck.return(cards)
+      player.trade_cards(cards, @deck.take(cards.count))
+    end
   end
 
   def end_round
+    show_hands
+    p "#{winner.hand} wins: $#{pot}"
+    p "winner hand: #{winner.hand.rank}"
+    winner.receive_winnings(pot)
+    @pot = 0
   end
 
   def winner
+    players.sort.last
   end
 
   def show_hands
+    puts "HANDS"
+    @players.each_with_index do |player, _|
+      next if player.folded?
+      puts "#{player.hand} (#{player.hand.rank})"
+    end
   end
 
   def add_to_pot(amount)
+    @pot += amount
+    amount
   end
 
   def game_over?
+    players.select { |player| !player.folded? }.count == 1
   end
 
   def add_players(n, buy_in)
+    n.times do
+      @players << Player.buy_in(buy_in)
+    end
   end
 end
 
--- w2/w2d3/poker/lib/player.rb
+++ w2/w2d3/poker/lib/player.rb
@@ -10,38 +10,78 @@ class Player
   end
 
   def deal_in(hand)
+    @hand = hand
   end
 
   def respond_bet
+    begin
+      print "action (call, bet, fold): "
+      action = gets.chomp
+      raise 'invalid action' unless ['call', 'bet', 'fold'].include?(action)
+      case action
+      when 'call' then :call
+      when 'bet' then :bet
+      when 'fold' then :fold
+      end
+    rescue
+      retry
+    end
   end
 
   def get_bet
+    begin
+      print "you have #{@bankroll}. how much to bet? "
+      bet = gets.chomp.to_i
+      raise 'not enough money' if bet > bankroll
+    rescue
+      retry
+    end
+
+    bet
   end
 
   def get_cards_to_trade
+    print "which cards do you want to trade? (3 max)"
+    card_idx = get.chomp.split(", ").map(&:to_i)
+    raise "cannot trade more than three cards" unless card_idx.length <= 3
+    card_idx.map { |idx| @hand.cards[idx] }
   end
 
   def take_bet(amount)
+    raise "not enough money" if amount > @bankroll
+    @bankroll -= amount
+    amount
   end
 
   def receive_winnings(amount)
+    @bankroll += amount
   end
 
   def return_cards
+    cards_to_return = @hand.cards
+    @hand = nil
+    cards_to_return
   end
 
   def fold
+    @folded = true
   end
 
   def unfold
+    @folded = false
   end
 
   def folded?
+    @bankroll == 0 || @folded
   end
 
   def trade_cards(old_cards, new_cards)
+    @hand.trade_cards(old_cards, new_cards)
   end
 
   def <=>(other_player)
+    p "your hand: #{@hand}"
+    p "other hand: #{other_player.hand}"
+    @hand <=> other_player.hand
   end
 end
