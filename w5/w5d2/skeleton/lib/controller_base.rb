2b4827fe07e9cfff8568ab858d2bd9380f6deda3
DanielLChang <daniellawrencechang@gmail.com>
2016-12-13 14:07:47 -0800

    Wrote session class

--- w5/w5d2/skeleton/lib/controller_base.rb
+++ w5/w5d2/skeleton/lib/controller_base.rb
@@ -8,25 +8,56 @@ class ControllerBase
 
   # Setup the controller
   def initialize(req, res)
+    @req = req
+    @res = res
+    @already_built_response = false
   end
 
   # Helper method to alias @already_built_response
   def already_built_response?
+    @already_built_response
   end
 
   # Set the response status code and header
   def redirect_to(url)
+
+    raise "double render error" if already_built_response?
+
+    @res.status = 302
+    @res['Location'] = url
+
+    @already_built_response = true
+
   end
 
   # Populate the response with content.
   # Set the response's content type to the given type.
   # Raise an error if the developer tries to double render.
   def render_content(content, content_type)
+
+    raise "double render error" if already_built_response?
+    
+    @res.write(content)
+    @res['Content-Type'] = content_type
+
+    @already_built_response = true
   end
 
   # use ERB and binding to evaluate templates
   # pass the rendered html to render_content
   def render(template_name)
+
+    path = File.dirname(__FILE__)
+
+    full_name = File.join(path, "..", "views",
+                          self.class.name.underscore,
+                          "#{template_name}.html.erb"
+                         )
+
+    template_code = ERB.new(File.read(full_name)).result(binding)
+
+    render_content(template_code, "text/html")
+
   end
 
   # method exposing a `Session` object
@@ -37,4 +68,3 @@ class ControllerBase
   def invoke_action(name)
   end
 end
-
